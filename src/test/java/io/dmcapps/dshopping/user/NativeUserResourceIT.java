03110b467d65a8fbd84b72f8ac3e298c23b24f0a
danielmachado86 <danielmcis@gmail.com>
2020-06-22 11:17:11 -0500

    Rest user bootstrapped

---
+++ src/test/java/io/dmcapps/dshopping/user/NativeUserResourceIT.java
@@ -0,0 +1,9 @@
+package io.dmcapps.dshopping.user;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeUserResourceIT extends UserResourceTest {
+
+    // Execute the same tests but in native mode.
+}
