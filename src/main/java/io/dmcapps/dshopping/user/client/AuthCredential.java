daa4ff77b0c8479d696cfdb7c5b523774130e587
danielmachado86 <danielmcis@gmail.com>
2020-06-26 21:08:21 -0500

    Auth service minimal refactor

--- src/main/java/io/dmcapps/dshopping/user/client/AuthCredential.java
+++ src/main/java/io/dmcapps/dshopping/user/client/AuthCredential.java
@@ -1,31 +1,11 @@
 package io.dmcapps.dshopping.user.client;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.jboss.logging.Logger;
 
 public class AuthCredential {
 
-    private static final Logger LOGGER = Logger.getLogger(
-        AuthCredential.class);
-
     public String type = "password";
     public String value;
     public boolean temporary = false;
-
-    @Override
-    public String toString() {
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonResult = null;
-        try {
-            jsonResult = mapper
-                .writeValueAsString(this);
-            
-        } catch ( JsonProcessingException  e) {
-            LOGGER.error(e.getMessage());
-        }
-        return jsonResult;
-    }
 
 }
--- src/main/java/io/dmcapps/dshopping/user/client/AuthUser.java
+++ src/main/java/io/dmcapps/dshopping/user/client/AuthUser.java
@@ -3,17 +3,9 @@ package io.dmcapps.dshopping.user.client;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.jboss.logging.Logger;
-
 import io.dmcapps.dshopping.user.User;
 
 public class AuthUser {
-
-    private static final Logger LOGGER = Logger.getLogger(
-        AuthUser.class);
 
     public String username;
     public String email;
@@ -24,20 +16,7 @@ public class AuthUser {
         this.username = user.email;
         this.email = user.email;
         this.enabled = true;
-        this.credentials.add(user.getCredential());
+        this.credentials.add(user.credential);
     }
 
-    @Override
-    public String toString() {
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonResult = null;
-        try {
-            jsonResult = mapper
-                .writeValueAsString(this);
-            
-        } catch ( JsonProcessingException  e) {
-            LOGGER.error(e.getMessage());
-        }
-        return jsonResult;
-    }
 }
