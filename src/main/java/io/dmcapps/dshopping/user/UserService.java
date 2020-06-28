444b5ac02945ef5b71f5da1f56973d2a14f04ed0
danielmachado86 <danielmcis@gmail.com>
2020-06-27 20:05:19 -0500

    added service methods

--- src/main/java/io/dmcapps/dshopping/user/UserService.java
+++ src/main/java/io/dmcapps/dshopping/user/UserService.java
@@ -3,8 +3,11 @@ package io.dmcapps.dshopping.user;
 import static javax.transaction.Transactional.TxType.REQUIRED;
 import static javax.transaction.Transactional.TxType.SUPPORTS;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 @ApplicationScoped
 @Transactional(REQUIRED)
@@ -15,9 +18,32 @@ public class UserService {
         return User.findById(id);
     }
 
+    @Transactional(SUPPORTS)
+    public List<User> findAllUsers() {
+        return User.listAll();
+    }
+
+    @Transactional(SUPPORTS)
+    public User findUserById(Long id) {
+        return User.findById(id);
+    }
+
+    public User updateUser(@Valid User user) {
+        User entity = User.findById(user.id);
+        entity.name = user.name;
+        entity.email = user.email;
+        entity.addresses = user.addresses;
+        entity.mobile = user.mobile;
+        return entity;
+    }
 
     public User persistUser(User user) {
         User.persist(user);
         return user;
+    }
+
+    public void deleteUser(Long id) {
+        User user = User.findById(id);
+        user.delete();
     }
 }
