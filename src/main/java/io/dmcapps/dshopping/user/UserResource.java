b45c8fe5f2d498fa8ad032e813fc4107440df824
danielmachado86 <danielmcis@gmail.com>
2020-06-27 20:05:46 -0500

    OpenAPI anotations added

--- src/main/java/io/dmcapps/dshopping/user/UserResource.java
+++ src/main/java/io/dmcapps/dshopping/user/UserResource.java
@@ -1,12 +1,20 @@
 package io.dmcapps.dshopping.user;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.util.Base64;
+import java.util.List;
+
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -16,14 +24,34 @@ import javax.ws.rs.core.UriInfo;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.openapi.annotations.tags.Tags;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.cache.NoCache;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 import io.dmcapps.dshopping.user.client.AuthService;
 import io.dmcapps.dshopping.user.client.AuthToken;
 import io.dmcapps.dshopping.user.client.AuthUser;
 
+@SecurityScheme(securitySchemeName = "jwt", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "jwt")
+@Tags(value = @Tag(name = "users", description = "All the user methods"))
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 @Path("/api/users")
 public class UserResource {
 
@@ -48,12 +76,55 @@ public class UserResource {
 
     @ConfigProperty(name = "quarkus.oidc.credentials.secret")
     String secret;
+    
+    @RolesAllowed("admin")
+    @SecurityRequirement(name = "jwt", scopes = {})
+    @Operation(summary = "Returns all users from the database")
+    @APIResponses(value = {
+        @APIResponse(responseCode = "401", description = "Unauthorized Error"),
+        @APIResponse(responseCode = "204", description = "No users"),
+        @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = User.class, type = SchemaType.ARRAY))),
+    })
+    @GET
+    public Response getAllUsers() {
+        List<User> users = service.findAllUsers();
+        LOGGER.debug("Total number of users " + users);
+        return Response.ok(users).build();
+    }
+    
+    @RolesAllowed("admin")
+    @SecurityRequirement(name = "jwt", scopes = {})
+    @Operation(summary = "Returns a user for a given identifier")
+    @APIResponses(value = {
+        @APIResponse(responseCode = "401", description = "Unauthorized Error"),
+        @APIResponse(responseCode = "204", description = "The user is not found for a given identifier"),
+        @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = User.class, type = SchemaType.ARRAY))),
+    })
+    @GET
+    @Path("/{id}")
+    public Response getUser(
+        @Parameter(description = "User identifier", required = true)
+        @PathParam("id") Long id) {
+        User user = service.findUserById(id);
+        if (user != null) {
+            LOGGER.debug("Found user " + user);
+            return Response.ok(user).build();
+        } else {
+            LOGGER.debug("No user found with id " + id);
+            return Response.noContent().build();
+        }
+    }
+
+    @RolesAllowed("user")
+    @SecurityRequirement(name = "jwt", scopes = {})
+    @Operation(summary = "Returns data for the authorized user")
+    @APIResponses(value = {
+        @APIResponse(responseCode = "401", description = "Unauthorized Error"),
+        @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = User.class, type = SchemaType.ARRAY))),
+    })
 
     @GET
     @Path("/me")
-    @RolesAllowed("user")
-    @Produces(MediaType.APPLICATION_JSON)
-    @NoCache
     public Response getUser() {
         String id = jwt.getSubject();
         User user = service.findUserById(id);
@@ -66,9 +137,12 @@ public class UserResource {
         }
     }
 
+    @Operation(summary = "Creates a valid user")
+    @APIResponse(responseCode = "201", description = "The URI of the created user", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = URI.class)))
     @POST
     public Response createUser(
-        User user, @Context UriInfo uriInfo) {
+        @RequestBody(required = true, content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = User.class)))
+        @Valid User user, @Context UriInfo uriInfo) {
         Response response = createAuthUser(user);
         user.id = getURI(response); // assign user id from auth user
         user = service.persistUser(user);
@@ -100,6 +174,37 @@ public class UserResource {
 			e.printStackTrace();
         }
         return "Basic " + headerValue;
+    }
+
+    @RolesAllowed("user")
+    @SecurityRequirement(name = "jwt", scopes = {})
+    @Operation(summary = "Updates an exiting  user")
+    @APIResponse(responseCode = "200", description = "The updated user", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = User.class)))
+    @PUT
+    @Path("/me")
+    public Response updateUser(
+        @RequestBody(required = true, content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = User.class)))
+        @Valid User user) {
+            user = service.updateUser(user);
+            LOGGER.debug("User updated with new valued " + user);
+            return Response.ok(user).build();
+        }
+        
+    @RolesAllowed("admin")
+    @SecurityRequirement(name = "jwt", scopes = {})
+    @Operation(summary = "Deletes an exiting user")
+    @APIResponses(value = {
+        @APIResponse(responseCode = "401", description = "Unauthorized Error"),
+        @APIResponse(responseCode = "204")
+    })
+    @DELETE
+    @Path("/{id}")
+    public Response deleteUser(
+        @Parameter(description = "User identifier", required = true)
+        @PathParam("id") Long id) {
+        service.deleteUser(id);
+        LOGGER.debug("User deleted with " + id);
+        return Response.noContent().build();
     }
     
     @GET
