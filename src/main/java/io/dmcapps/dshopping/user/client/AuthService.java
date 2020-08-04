377df69aa1fdafb098fa5c37d926fc64f69ab2c0
danielmachado86 <danielmcis@gmail.com>
2020-08-03 23:03:00 -0500

    keycloak realm name changed

--- src/main/java/io/dmcapps/dshopping/user/client/AuthService.java
+++ src/main/java/io/dmcapps/dshopping/user/client/AuthService.java
@@ -16,13 +16,13 @@ import javax.ws.rs.core.Response;
 public interface AuthService {
 
     @POST
-    @Path("/realms/quarkus/protocol/openid-connect/token")
+    @Path("/realms/dshopping/protocol/openid-connect/token")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
     public AuthToken getToken(@HeaderParam("Authorization") String authorization, @FormParam("grant_type") String grant_type);
     
     @POST
-    @Path("/admin/realms/quarkus/users")
+    @Path("/admin/realms/dshopping/users")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
     public Response createUser(@HeaderParam("Authorization") String authorization, AuthUser authUser);
